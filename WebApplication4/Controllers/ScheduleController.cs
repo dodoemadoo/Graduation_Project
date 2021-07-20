6f5a638fb21a911aa8710bbeb95e3dda140e8863
dodoemadoo <36114079+dodoemadoo@users.noreply.github.com>
2021-07-20 13:06:04 +0200

    .

--- WebApplication4/Controllers/ScheduleController.cs
+++ WebApplication4/Controllers/ScheduleController.cs
@@ -18,7 +18,7 @@ namespace WebApplication4.Controllers
                 var firstCondition = schedule.schedules.Where(s => s.slot_ID == slot.slot_ID && s.week_Day == slot.week_Day && s.class_ID == slot.class_ID).ToList();
                 var secondCondition = schedule.schedules.Where(s => s.slot_ID == slot.slot_ID && s.week_Day == slot.week_Day && s.teacher_subject_ID == slot.teacher_subject_ID).ToList();
                 var thirdCondition = schedule.schedules.Where(s => s.class_ID == slot.class_ID && s.teacher_subject_ID == slot.teacher_subject_ID).ToList();
-                if (firstCondition.Count == 0 || secondCondition.Count == 0 || thirdCondition.Count == 0)
+                if (firstCondition.Count != 0 || secondCondition.Count != 0 || thirdCondition.Count != 0)
                     return Request.CreateResponse(HttpStatusCode.BadRequest, false);
                 else
                     return Request.CreateResponse(HttpStatusCode.OK, true);
@@ -43,8 +43,36 @@ namespace WebApplication4.Controllers
                 }
                else
                     return Request.CreateResponse(HttpStatusCode.BadRequest, "Cannot create this slot");
-               
-                //return Request.CreateResponse(HttpStatusCode.OK, "");
+            }
+        }
+
+        [HttpPut]
+        [Route("api/Schedule/Update")]
+        public HttpResponseMessage updateSlot(int slot_id, [FromBody] schedule slot)
+        {
+            try
+            {
+                using (ScheduleEntities schedule = new ScheduleEntities())
+                {
+                    var entity = schedule.schedules.FirstOrDefault(s => s.slot_ID  == slot_id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Mark with id = " + slot_id.ToString() + " not found to update");
+                    }
+                    else
+                    {
+                        entity.week_Day = slot.week_Day;
+                        entity.slot_ID = slot.slot_ID;
+                        entity.class_ID = slot.class_ID;
+                        entity.teacher_subject_ID = slot.teacher_subject_ID;
+                        schedule.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
         }
     }
